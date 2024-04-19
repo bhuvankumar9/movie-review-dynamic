c1dd7b8d3b440d38069e33f27715a3c3b44b424b
bhuvankumar9 <bhuvankumar155@gmail.com>
2024-04-19 15:12:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import app from "./server.js";
+import mongodb from "mongodb";
+ import ReviewsDAO from "./dao/reviewsDAO.js"
+const MongoClient = mongodb.MongoClient;
+ const mongo_username = process.env['MONGO_USERNAME']
+ const mongo_password = process.env['MONGO_USERNAME']
+
+ ///const uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.3xy0j1t.mongodb.net/?retryWrites=false&appName=Cluster0`;
+ const uri = `mongodb+srv://bhuvan:hYoMkk3lZnwnSfPA@cluster0.3xy0j1t.mongodb.net/?retryWrites=false&appName=Cluster0`;
+
+ const port = 8000; 
+
+ MongoClient.connect(
+    uri,
+    {
+      maxPoolSize: 50,
+      wtimeoutMS: 2500,
+    //   useNewUrlParser: true  
+    }
+ )
+ .catch(err => {
+    console.error(err.stack)
+    process.exit(1)
+ })
+ .then(async client => {
+    await ReviewsDAO.injectDB(client)
+    app.listen(port, () => {
+        console.log(`listining on port ${port}`);
+    })
+ })
+
+
+
+
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+reviewsDAO.js
